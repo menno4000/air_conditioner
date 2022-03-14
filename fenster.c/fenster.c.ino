6808dc9b99df2f11cb617dc7b613d71c36a308da
menno4000 <maxluedemann@t-online.de>
2022-03-14 21:24:48 +0100

    fenster 3 point switch control

--- fenster.c/fenster.c.ino
+++ fenster.c/fenster.c.ino
@@ -9,6 +9,43 @@ VL53L0X sensor;
 int IN1 = 7;
 int IN2 = 8;
 
+// Define 3 position slide switch pins
+int IN3 = 2;
+int IN4 = 3;
+
+// state for the window automaton controlled by three way switch
+// 0: window is kept closed
+// 1: window is kept open
+// 2: window is waiting for ToF input
+int automationState = 2;
+// state to keep track of automation phases 
+// 0: window needs to be opened
+// 1: window needs to rest
+// 2: window needs to close
+// 3: window is waiting for more input
+int automationPhase = 3;
+
+// state for motor controller
+// 0: motor is retreating
+// 1: motor is resting
+// 2: motor is extending
+int motorState = 1;
+
+// state of the window
+// 0: window is closed
+// 1: window is open
+int windowState = 0;
+
+// time for window operations in frames of 200ms
+const int retreatTime = 15;
+const int extendTime = 15;
+const int airTime = 15;
+
+// counter for the window operations 
+int windowCounter = 0;
+// routine activity flag for window operations
+bool isRunning = false;
+
 void setup()
 {
   Serial.begin(9600);
@@ -27,198 +64,203 @@ void setup()
   // instead, provide a desired inter-measurement period in
   // ms (e.g. sensor.startContinuous(100)).
   sensor.startContinuous();
+  Serial.println("Initialized ToF sensor.");
 
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
 
+  pinMode(IN3, INPUT_PULLUP);
+  pinMode(IN4, INPUT_PULLUP);
+
+  Serial.println("Setting timer.");
+
   cli();               // stop interrupts for settings
   TCCR1A = 0;          // clear timer 1 registers
   TCCR1B = 0;
-  TCCR1A |= B00000100; // set prescalar to 256
-  TIMSK1 |= B00000010; // enable compare match on register A
+  TCNT1 = 0;
+  // TCCR1A |= B00000100; // set prescalar to 256
+  // TIMSK1 |= B00000010; // enable compare match on register A
   OCR1A = 12500;       // set timer to trigger at 200ms
+
+  TCCR1B |= (1 << WGM12);
+  // Set CS10 and CS12 bits for 1024 prescaler
+  TCCR1B |= (1 << CS12) | (1 << CS10);  
+  // enable timer compare interrupt
+  TIMSK1 |= (1 << OCIE1A);
   sei();               // re-enable interrupts
 
-  // state for the window automaton controlled by three way switch
-  // 0: window is kept closed
-  // 1: window is kept open
-  // 2: window is waiting for ToF input
-  int automationState = 1;
-  // state to keep track of automation phases 
-  // 0: window needs to be opened
-  // 1: window needs to rest
-  // 2: window needs to close
-  // 3: window is waiting for more input
-  int automationPhase = 3;
-  
-  // state for motor controller
-  // 0: motor is retreating
-  // 1: motor is resting
-  // 2: motor is extending
-  int motorState = 1;
-
-  // state of the window
-  // 0: window is closed
-  // 1: window is open
-  int windowState = 0;
-  
-  // time for window operations in frames of 200ms
-  const int retreatTime = 15;
-  const int extendTime = 15;
-  const int airTime = 15;
-
-  // counter for the window operations 
-  int windowCounter = 0;
-  // routine activity flag for window operations
-  bool isRunning = false;
+  Serial.println("timer set.");
 }
 
 void loop()
 {
-
-  ISR(TIMER1_COMPA_vect) {
-    TCNT1 = 0;            // reset timer 1
-    if (automationState == 3) {
-      // assure that window is closed
-      // maybe this could trigger a warning LED
-      if (windowState != 0) {
-        windowCounter++;
-        if (motorState != 2) {
-          if (windowCounter =< extendTime) {
-            digitalWrite(IN1, LOW);
-            digitalWrite(IN2, HIGH);
-            motorState = 2;
-          }
-          else {
-            digitalWrite(IN1, LOW);
-            digitalWrite(IN2, LOW);
-            motorState = 1;
-            windowState = 0;
-            windowCounter = 0;
-          }
+  if (digitalRead(IN3) == HIGH) {
+    if(digitalRead(IN4) == HIGH) {
+      //Serial.println("I'm in mode 0! Window is kept closed.");
+      automationState = 0;
+    } else {
+      //Serial.println("I'm in mode 1! Window is kept open.");
+      automationState = 1;
+    }
+  } else {
+    //Serial.println("I'm in mode 2! Window is operated by the chip.");
+    if (automationState == 1){
+      automationState == 3;
+    }
+    else {
+      automationState == 2;
+    }
+  }
+    
+  if (automationState == 3) {
+    // assure that window is closed
+    // maybe this could trigger a warning LED
+    if (windowState != 0) {
+      if (motorState != 2) {
+        if (windowCounter < extendTime) {
+          digitalWrite(IN1, LOW);
+          digitalWrite(IN2, HIGH);
+          motorState = 2;
         }
-      } 
-      // regular ToF sensor operation
-      else {
-        int distanceRead = sensor.readRangeContinuousMillimeters();
-        if (sensor.timeoutOccurred()) {
-          // this could switch on a LED displaying sensor conditions
-          Serial.print(" TIMEOUT");
-        } else {
-          Serial.println(distanceRead);
-          // run routine once ToF sensor is triggered, but only as long as no routine is in progress
-          if (distanceRead < 1000) {
-            if (automationPhase == 3) {
-              Serial.println("poop time");
-              automationPhase = 0;
-            }
+        else {
+          digitalWrite(IN1, LOW);
+          digitalWrite(IN2, LOW);
+          motorState = 1;
+          windowState = 0;
+          windowCounter = 0;
+          automationState = 2;
+        }
+      }
+    } 
+  }
+    // regular ToF sensor operation
+  if (automationState == 2){
+     if (isRunning == false) {
+      // Serial.println("Waiting for a shit!");
+      int distanceRead = sensor.readRangeContinuousMillimeters();
+      if (sensor.timeoutOccurred()) {
+        // this could switch on a LED displaying sensor conditions
+        Serial.print(" TIMEOUT");
+      } else {
+        // Serial.println(distanceRead);
+        // run routine once ToF sensor is triggered, but only as long as no routine is in progress
+        if (distanceRead < 1000) {
+          if (automationPhase == 3) {
+            // Serial.println("poop time");
+            automationPhase = 0;
             isRunning = true;
+            // Serial.println("Opening dat dere window");
+          } else {
+            // Serial.println("Window already in motion");
           }
         }
-        // routine for air conditiong
-        if (isRunning == true) {
-          windowCounter++;
-          // window opening
-          if (automationPhase == 0) {
-            if (windowCounter =< retreatTime) {
-              if (motorState != 0) {
-                digitalWrite(IN1, LOW);
-                digitalWrite(IN2, HIGH);
-                motorState = 0;
-              }
-            } else {
-              digitalWrite(IN1, LOW);
-              digitalWrite(IN2, LOW);
-              motorState = 1;
-              automationPhase = 1;
-              windowCounter = 0;
-            }
-          }
-          // window resting
-          if (automationPhase == 1) {
-            if (windowCounter == airTime) {
-              automationPhase = 2;
-              windowCounter = 0;
-            }
-          }
-          // window closing
-          if (automationPhase == 2) {
-            if (windowCounter =< extendTime) {
-              if (motorState != 2) {
-                digitalWrite(IN1, HIGH);
-                digitalWrite(IN2, LOW);
-                motorState = 2;
-              }
-            } else {
-              digitalWrite(IN1, LOW);
-              digitalWrite(IN2, LOW);
-              motorState = 1;
-              automationPhase = 3;
-              windowCounter = 0;
-              isRunning = false;
-            }
-          }
-        }
-      }    
-    } 
-    // control override state = closed
-    else if (automationState == 0) {
-       if (windowState != 0) {
-        windowCounter++;
-        if (motorState != 2) {
-          if (windowCounter =< extendTime) {
+      }
+      // routine for air conditiong
+    } else {
+      // window opening
+      if (automationPhase == 0) {
+        if (windowCounter < retreatTime) {
+          if (motorState != 0) {
             digitalWrite(IN1, LOW);
             digitalWrite(IN2, HIGH);
-            motorState = 2;
+            motorState = 0;
+            windowState = 1;
           }
-          else {
-            digitalWrite(IN1, LOW);
-            digitalWrite(IN2, LOW);
-            motorState = 1;
-            windowState = 0;
-            windowCounter = 0;
-          }
+        } else {
+          //Serial.println("We breezy!");
+          digitalWrite(IN1, LOW);
+          digitalWrite(IN2, LOW);
+          motorState = 1;
+          automationPhase = 1;
+          windowCounter = 0;
         }
-      } 
-    } 
-    // controlled override state: open
-    else if (automationState == 1) {
-        if (windowState != 0) {
-        windowCounter++;
-        if (motorState != 2) {
-          if (windowCounter =< extendTime) {
+      }
+      // window resting
+      if (automationPhase == 1) {
+        if (windowCounter == airTime) {
+          //Serial.println("We done here!");
+          automationPhase = 2;
+          windowCounter = 0;
+          //Serial.println("Closing the window.");
+        }
+      }
+      // window closing
+      if (automationPhase == 2) {
+       
+        if (windowCounter < extendTime) {
+          if (motorState != 2) {
             digitalWrite(IN1, HIGH);
             digitalWrite(IN2, LOW);
             motorState = 2;
           }
-          else {
-            digitalWrite(IN1, LOW);
-            digitalWrite(IN2, LOW);
-            motorState = 1;
-            windowState = 0;
-            windowCounter = 0;
-          }
+        } else {
+          Serial.println("We dun here!");
+          digitalWrite(IN1, LOW);
+          digitalWrite(IN2, LOW);
+          windowState = 0;
+          motorState = 1;
+          automationPhase = 3;
+          windowCounter = 0;
+          isRunning = false;
+          //Serial.println("Waiting for the next shit...");
         }
-      } 
+      }
+    }
+  }    
+   
+  // control override state = closed
+  // TODO add reaction to button
+  if (automationState == 0) {
+     if (windowState != 0) {
+      if (motorState != 2) {
+        if (windowCounter < extendTime) {
+          digitalWrite(IN1, LOW);
+          digitalWrite(IN2, HIGH);
+          motorState = 2;
+        }
+        else {
+          digitalWrite(IN1, LOW);
+          digitalWrite(IN2, LOW);
+          motorState = 1;
+          windowState = 0;
+          windowCounter = 0;
+        }
+      }
+    } else { 
+      windowCounter = 0;
+    }
+  } 
+  // controlled override state: open
+  // TODO add reaction to button
+  if (automationState == 1) {
+      if (windowState != 1) {
+      if (motorState != 2) {
+        if (windowCounter < extendTime) {
+          digitalWrite(IN1, HIGH);
+          digitalWrite(IN2, LOW);
+          motorState = 2;
+          windowState = 1;
+        }
+        else {
+          digitalWrite(IN1, LOW);
+          digitalWrite(IN2, LOW);
+          motorState = 1;
+          windowCounter = 0;
+        }
+      }
+    } else { 
+      windowCounter = 0;
     }
   }
+  
+}
 
 
-  {
-    Serial.print("pooptime \n");
-    digitalWrite(IN1, HIGH);
-    digitalWrite(IN2, LOW);
-    delay(3000);
-    digitalWrite(IN1, LOW);
-    digitalWrite(IN2, LOW);
-    //delay(900000);
-    delay(2000);
-    digitalWrite(IN1, LOW);
-    digitalWrite(IN2, HIGH);
-    delay(3000);
-    digitalWrite(IN1, LOW);
-    digitalWrite(IN2, LOW);
-    Serial.print("heat time \n");
-  }
-  delay(200);
+ISR(TIMER1_COMPA_vect) {
+  TCNT1 = 0;            // reset timer 1
+  windowCounter ++;
+  
+  Serial.println("timer moment.");
+  Serial.println(automationState);
+  
 }
